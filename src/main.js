d42782089c58b0c23687702db3630f27d9b44ae6
Sekori1 <romaric55220@gmail.com>
2020-01-18 12:28:14 +0100

    Fix le bug

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import App from './App.vue'
-import Home from './components/Home.vue'
-
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   routes: [{
     path: '/',
+    name: 'Home',
     component: Home
   }]
 })
 
+import App from './App.vue'
+import Home from './components/Home.vue'
+
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
