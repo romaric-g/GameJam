0554abbf4afc0a61613ad74ce46d37781d5ef0d7
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2020-01-18 16:57:59 +0100

    graphique page d'accueil

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="Ajout">
-    <label for="pseudo">Ajouter un joueur :</label><br>
-    <tr>
-      <td>
-        <input type="text" placeholder="Ex: EMMI"/>
-      </td>
-      <td>
-        <input type="submit" value="+">
-      </td>
-    </tr>
-    <span class="partie">
-    <input type="submit" value="Commencer la partie" />
-  </span>
+  <div class="">
+    <div class="add">
+      <div class="text">
+        <label>Ajouter un </br>joueur
+        <hr width="60%" size="1" color="#ef476f" align="left">
+      </label>
+      </div>
+      <div class="adding">
+        <input type="text" placeholder="Ex: EMMI" class="barre"/>
+        <input type="submit" value="+" class="bouton" />
+      </div>
+    </div>
+    <div class="start">
+      <input @click="changePage()" type="submit" value="Commencer la partie" class="start-bouton"/>
+    </div>
   </div>
 </template>
 
@@ -31,22 +33,70 @@ export default {
     },
     Launch: function(event) {
       store.dispatch('',this.form.username);
-      router.push('/Game');
+      router.push('/Play');
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+.add{
+  padding-top: 40%;
+  display:flex;
+  flex-direction: column;
+}
+
+.text{
+  display:flex;
+  align-items: left;
+}
+
+.adding{
+  display:flex;
+  flex-direction: line;
+  align-items: center;
+  margin-left: 3%
+}
+
+.start{
+  display:flex;
+  align-items: center;
+  margin-top: 75%;
+  margin-left: 3%
+}
+
+.barre{
+  font-size: 350%;
+  width: 90%;
+  margin: 1em 0;
+}
+
 label {
-  font-size: 100px;
+  font-size: 80px;
   color: white;
 }
 
-input submit {
-  background-color: #ef476f
+hr {
+  color: #ef476f;
+  margin: -0.1em 0;
 }
-.partie {
-  text-align: center
+
+.bouton {
+  background-color: #ef476f;
+  Color: white;
+  font-size: 400%;
+  border: none;
+}
+
+.start-bouton {
+  background-color: #ef476f;
+  Color: white;
+  font-size: 400%;
+  font-weight: bold;
+  border-radius: 3px;
+  cursor: pointer;
+  border: none;
+  text-decoration: none;
 }
 </style>
